4b7648be06bac14d9be9484e17f10ccc1d3c726a
MacLean Koslowski <macleankoslowski@MacLeans-MacBook-Pro.local>
2022-06-09 16:02:50 -0400

    added react project for easier use of firebase

---
+++ frontend/bizgen-frontend/src/App.js
@@ -0,0 +1,69 @@
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './bizgen_logo.png'
+import Home from './views/Home';
+import Blog from './views/Blog';
+import Contact from './views/Contact';
+import About from './views/About';
+import Business from './views/Business';
+import {LinkContainer} from 'react-router-bootstrap';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <div >
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <LinkContainer to='/'>
+                  <Nav.Link >Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/about'>
+                  <Nav.Link >About</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/business'>
+                  <Nav.Link >Business Loans</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/blog'>
+                  <Nav.Link >Blog</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/contact'>
+                  <Nav.Link >Contact</Nav.Link>
+                </LinkContainer>
+                
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/business" element={<Business/>}/>
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/bizgen-frontend/src/views/Business.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Business extends React.Component {
+    render() {
+      return (
+        <div>
+          Business
+        </div>
+      );
+    }
+}
+
+export default Business;
